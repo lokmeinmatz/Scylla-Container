e83fd462eebe4f8a409dac55828f470a5900f360
agottwald02 <116649511+agottwald02@users.noreply.github.com>
2023-01-25 15:16:48 +0100

    Update ScyllaScript.sh

--- ScyllaScript.sh
+++ ScyllaScript.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/bash
 
 cd scylla-dev_ui
-java -cp "target/classes;../dependencies/*;*" de.hpi.bpt.scylla.Scylla --config=samples/Kreditkarte_global_1.xml --bpmn=samples/Kreditkarte_1.bpmn --sim=samples/Kreditkarte_sim_1.xml 
+java -cp "target/classes;../dependencies/*;lib/*;*" de.hpi.bpt.scylla.Scylla --config=samples/Kreditkarte_global_1.xml --bpmn=samples/Kreditkarte_1.bpmn --sim=samples/Kreditkarte_sim_1.xml --enable-bps-logging
