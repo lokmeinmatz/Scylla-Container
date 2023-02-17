1c3512a31afa1d3b2d30d51cf47fbd736c8043e4
schleypen1610 <schleypen@campus.tu-berlin.de>
2023-02-17 18:10:11 +0100

    continued sending and receiving

--- apiTool.py
+++ apiTool.py
@@ -1,7 +1,7 @@
 import json
 from os.path import isfile, join
 
-from flask import Flask, request
+from flask import Flask, request, send_file
 from flask_restful import Resource, Api, abort
 import subprocess
 from datetime import datetime
@@ -30,49 +30,7 @@ api = Api(app)
 
 
 
-#create project directory:
-#projectDir = os.path.join('projects',str(datetime.now()).replace(" ", "T").replace(':', '_'))  #TODO activate those two lines
-#os.mkdir(projectDir)
-projectDir = os.path.join('projects', 'testp')  #TODO: deactivate
 
-#get filename for file from PetriSim
-for f in inDirectory(projectDir):
-    if f.endswith('.json'):
-        convInputFile = os.path.join('..', projectDir, f)
-
-# run converter
-subprocess.call("node scyllaConverter/ConvertMain.js " + convInputFile + " " + projectDir, shell=True)
-
-# input of Scylla <- output of Scylla Converter:
-for f in inDirectory(projectDir):
-    if f.endswith('.bpmn'):
-        bpmn = os.path.join('..', projectDir, f)
-    elif f.endswith('Global.xml'):
-        globConfig = os.path.join('..', projectDir, f)
-    elif f.endswith('Sim.xml'):
-        simConfig = os.path.join('..', projectDir, f)
-
-# run Scylla:
-beforeList = inDirectory(projectDir)
-subprocess.call(['bash', "ScyllaScript2.sh", '--config=' + globConfig, '--bpmn=' + bpmn, '--sim=' + simConfig])
-afterList = inDirectory(projectDir)
-
-# new folder created from Scylla:
-newInDir = listCompare(beforeList, afterList)
-
-if len(newInDir) == 1:
-    newScyllaOutFolder = newInDir.pop()
-else:
-    raise Exception("More folders than one created by scylla!")
-
-# get filenames created from Scylla:
-newScyllaFiles = fileInDirectory(join(projectDir, newScyllaOutFolder))
-
-print(newScyllaFiles)   #TODO: return to PetriSim
-# def write_changes_to_file():
-#    with open('storedData.json', 'w') as w:
-#        json.dump(storedData, w)
-#
 
 
 class DataOverview(Resource):
@@ -83,15 +41,64 @@ class DataOverview(Resource):
 
     def post(self):
         data_from_request = request.data.decode('UTF-8')
-        json_object = json.loads(data_from_request)
-        new_data = {'key1': json_object['key1']}
-        data_id = 'data' + str(max(int(v.lstrip('data')) for v in storedData.keys()) + 1)
+        #json_object = json.loads(data_from_request)
+
+        # create project directory:
+        #projectDir = os.path.join('projects',str(datetime.now()).replace(" ", "T").replace(':', '_'))  #TODO activate those two lines
+        #os.mkdir(projectDir)
+
+        projectDir = os.path.join('projects', 'testp')  # TODO: deactivate
+
+        newFile = open(os.path.join(projectDir,"pizza_1.json"), "x") # TODO: getName from request
+        newFile.write(data_from_request)
+        newFile.close()
+
+        # get filename for file from PetriSim
+        for f in inDirectory(projectDir):
+            if f.endswith('.json'):
+                convInputFile = os.path.join('..', projectDir, f)
+
+        # run converter
+        subprocess.call("node scyllaConverter/ConvertMain.js " + convInputFile + " " + projectDir, shell=True)
+
+        # input of Scylla <- output of Scylla Converter:
+        for f in inDirectory(projectDir):
+            if f.endswith('.bpmn'):
+                bpmn = os.path.join('..', projectDir, f)
+            elif f.endswith('Global.xml'):
+                globConfig = os.path.join('..', projectDir, f)
+            elif f.endswith('Sim.xml'):
+                simConfig = os.path.join('..', projectDir, f)
+
+        # run Scylla:
+        beforeList = inDirectory(projectDir)
+        subprocess.call(['bash', "ScyllaScript2.sh", '--config=' + globConfig, '--bpmn=' + bpmn, '--sim=' + simConfig])
+        afterList = inDirectory(projectDir)
+
+        # new folder created from Scylla:
+        newInDir = listCompare(beforeList, afterList)
+
+        if len(newInDir) == 1:
+            newScyllaOutFolder = newInDir.pop()
+        else:
+            raise Exception("More folders than one created by scylla!")
+
+        # get filenames created from Scylla:
+        newScyllaFiles = fileInDirectory(join(projectDir, newScyllaOutFolder))
+
+        print(newScyllaFiles)  # TODO: return to PetriSim
+        # def write_changes_to_file():
+        #    with open('storedData.json', 'w') as w:
+        #        json.dump(storedData, w)
+        #
 
         print("This is a POST request. Instead of printing we could do sth else")
         # start converter with body from request
+        files = {'file1': open('hello.py', 'rb'), 'file2': open('hello2.py', 'rb')}
+        #return str(open(os.path.join(projectDir,"pizza_1.json"), "r"))
+        return send_file() or send_from_directory
 
 
-        return storedData[data_id], 201
 
 api.add_resource(DataOverview, '/storeddata')
 
